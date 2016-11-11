fd21354a07b23f79b24f5618b70733e7119b72a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-11-11 05:54:01 +0100

    Add eu_ES, thanks to bixerdo

---
+++ language/eu_ES/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_webmaster_only'] = 'Web-arduradunek baino ezin dituzte sortu edo aldatu fitxategi lokalak.';
+$lang['locfiledit_syntax_error'] = 'Sintaxi-errorea! Ezin da fitxategia gorde.';
+$lang['locfiledit_show_default'] = 'Erakutsi erreferentzia-fitxategia:';
+$lang['locfiledit_saved_bak'] = 'Sortu egin da babeskopia fitxategia (%s)';
+$lang['locfiledit_save_file'] = 'Gorde fitxategia';
+$lang['locfiledit_save_config'] = 'Fitxategia ondo idatzi da.';
+$lang['locfiledit_save_bak'] = 'Babeskopia sortuko da gordetzerakoan.';
+$lang['locfiledit_restore_confirm'] = 'Baieztatu?
+Leheneratzea ez da eraginkorra izango hurrengo gordetzera arte.';
+$lang['locfiledit_restore'] = 'Leheneratu babeskopia-fitxategia';
+$lang['locfiledit_parent_directory'] = 'Direktorio nagusia';
+$lang['locfiledit_onglet_tpl'] = 'Txantiloiak';
+$lang['locfiledit_onglet_plug'] = 'Plugin pertsonala';
+$lang['locfiledit_onglet_localconf'] = 'Konfigurazio lokala';
+$lang['locfiledit_onglet_lang'] = 'Hizkuntzak';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_newfile'] = 'Ez da fitxategirik izango behealdean informazioa idatzi arte. Behin informazioa idatzita eta gordeta, fitxategia sortu egingo da.';
+$lang['locfiledit_new_tpl'] = 'Sortu fitxategi berria';
+$lang['locfiledit_new_filename'] = 'Fitxategiaren izena';
+$lang['locfiledit_model_error'] = 'Eredu bat aukeratu behar duzu.';
+$lang['locfiledit_model'] = 'Eredua';
+$lang['locfiledit_filename_error'] = 'Debekatutako karaktereak fitxategiaren izenean.';
+$lang['locfiledit_file_already_exists'] = 'Fitxategia dagoeneko existitzen da.';
+$lang['locfiledit_empty_page'] = 'Orria hutsik';
+$lang['locfiledit_empty_filename'] = 'Fitxategiaren izena idatzi behar duzu.';
+$lang['locfiledit_edit'] = 'Editatu';
+$lang['locfiledit_choose_file'] = 'Aukeratu editatu nahi duzun fitxategia';
+$lang['locfiledit_cant_save'] = 'Uneko fitxategia ezin daiteke idatzi. Egiaztatu "local/" direktorioan idatz daitekeen (chmod).';
+$lang['locfiledit_bak_loaded2'] = 'Fitxategia leheneratzeko aurretik gorde egin behar duzu.';
+$lang['locfiledit_bak_loaded1'] = 'Babeskopia-fitxategia kargatuta';
