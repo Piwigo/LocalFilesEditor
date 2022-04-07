4469ad2f6b3bcd9f2869b3176610d4dc41159381
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-07 17:31:42 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_parent_directory'] = 'Foreldramappa';
+$lang['locfiledit_restore'] = 'Endurheimta afritunarskrána';
+$lang['locfiledit_restore_confirm'] = 'Vinsamlega staðfestu?
+Endurheimt mun ekki taka gildi fyrr en við næstu vistun.';
+$lang['locfiledit_save_bak'] = 'Afritunareintak verður útúið við næstu vistun.';
+$lang['locfiledit_save_config'] = 'Skrá vistuð giftusamlega.';
+$lang['locfiledit_save_file'] = 'Vista skrá';
+$lang['locfiledit_saved_bak'] = 'Afritunarskrá útbúin (%s)';
+$lang['locfiledit_show_default'] = 'Sýna viðmiðunarskrá:';
+$lang['locfiledit_syntax_error'] = 'Setningarvilla! Ekki er hægt að vista skrána.';
+$lang['locfiledit_webmaster_only'] = 'Aðeins vefstjórar geta stofnað eða breytt staðbundnum skrám.';
+$lang['Customize CSS'] = 'Sérsníða CSS';
+$lang['locfiledit_bak_loaded1'] = 'Afritaskrá innflutt.';
+$lang['locfiledit_bak_loaded2'] = 'Þú verður að vista skrána til að endurheimta hana.';
+$lang['locfiledit_cant_save'] = 'Núverandi skrá er ekki skrifanleg. Athugaðu hvort skráin "local/" sé skrifanleg (chmod).';
+$lang['locfiledit_choose_file'] = 'Veldu skránna sem á að breyta';
+$lang['locfiledit_edit'] = 'Breyta';
+$lang['locfiledit_empty_filename'] = 'Þú verður að slá inn skráarheitið.';
+$lang['locfiledit_empty_page'] = 'Tóm síða';
+$lang['locfiledit_file_already_exists'] = 'Skrá er þegar til.';
+$lang['locfiledit_filename_error'] = 'Bannaðir stafir í skráarheiti.';
+$lang['locfiledit_model'] = 'Fyrirmynd';
+$lang['locfiledit_model_error'] = 'Þú verður að velja fyrirmynd.';
+$lang['locfiledit_new_filename'] = 'Skráarheiti';
+$lang['locfiledit_new_tpl'] = 'Stofna nýja skrá';
+$lang['locfiledit_newfile'] = 'Skráin er ekki til fyrr en einhverjar upplýsingar eru færðar inn
+fyrir neðan. Þegar upplýsingar hafa verið slegnar inn og vistaðar verður skráin búin til.';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Tungumál';
+$lang['locfiledit_onglet_localconf'] = 'Staðbundin uppsetning';
+$lang['locfiledit_onglet_plug'] = 'Persónuleg viðbót';
+$lang['locfiledit_onglet_tpl'] = 'Sniðmát';
