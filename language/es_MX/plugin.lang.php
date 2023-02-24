fe01f7da6bfcb1e9933b33739d6033cefc02884f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-24 20:46:45 +0100

    Update es_MX, thanks to grantiago

---
+++ language/es_MX/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_parent_directory'] = 'Directorio padre';
+$lang['locfiledit_restore'] = 'Restaurar el archivo de respaldo';
+$lang['locfiledit_restore_confirm'] = 'Por favor confirmar? La restauración no será efectiva hasta el próximo guardado.';
+$lang['locfiledit_save_bak'] = 'Se creará un respaldo al guardar.';
+$lang['locfiledit_save_config'] = 'Archivo escrito correctamente.';
+$lang['locfiledit_save_file'] = 'Guardar el archivo';
+$lang['locfiledit_saved_bak'] = 'Respaldo creado (%s)';
+$lang['locfiledit_show_default'] = 'Mostrar archivo de referencia:';
+$lang['locfiledit_syntax_error'] = '¡Error de sintaxis! El archivo no se puede guardar.';
+$lang['locfiledit_webmaster_only'] = 'Solo los webmasters pueden crear o modificar archivos locales.';
+$lang['Customize CSS'] = 'Personalizar CSS';
+$lang['locfiledit_bak_loaded1'] = 'Archivo de respaldo cargado.';
+$lang['locfiledit_bak_loaded2'] = 'Debe guardar el archivo para restaurarlo.';
+$lang['locfiledit_cant_save'] = 'El archivo actual no se puede escribir. Compruebe si el directorio "local/" se puede escribir (chmod).';
+$lang['locfiledit_choose_file'] = 'Elija el archivo a editar';
+$lang['locfiledit_edit'] = 'Editar';
+$lang['locfiledit_empty_filename'] = 'Debe ingresar el nombre del archivo.';
+$lang['locfiledit_empty_page'] = 'Página vacía';
+$lang['locfiledit_file_already_exists'] = 'El archivo ya existe.';
+$lang['locfiledit_filename_error'] = 'Caracteres prohibidos en el nombre del archivo.';
+$lang['locfiledit_model'] = 'Modelo';
+$lang['locfiledit_model_error'] = 'Debes elegir un modelo.';
+$lang['locfiledit_new_filename'] = 'Nombre del archivo';
+$lang['locfiledit_new_tpl'] = 'Crear nuevo archivo';
+$lang['locfiledit_newfile'] = 'El archivo no existe hasta que se ingresa alguna información abajo. Una vez ingresada y guardada la información, se creará el archivo.';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Idiomas';
+$lang['locfiledit_onglet_localconf'] = 'Configuración local';
+$lang['locfiledit_onglet_plug'] = 'Plugin personal';
+$lang['locfiledit_onglet_tpl'] = 'Plantillas';
