4e107b6f27a2b9eefc7cfe20ade4fdd2b122333f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-05-03 20:48:17 +0200

    Update fa_IR, thanks to Garcia

--- language/fa_IR/plugin.lang.php
+++ language/fa_IR/plugin.lang.php
@@ -21,34 +21,33 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['locfiledit_bak_loaded1'] = 'فایل پشتیبان بارگذاری شد.';
-$lang['locfiledit_bak_loaded2'] = 'برای بازیابی فایل باید آن را ذخیره کنید.';
-$lang['locfiledit_cant_save'] = 'این فایل قابل ویرایش نمی باشد. باید دسترسی (chmod) آن را از داخل هاست تغییر دهید.';
+$lang['locfiledit_bak_loaded2'] = 'برای بازیابی فایل، باید ابتدا آن را ذخیره کنید.';
+$lang['locfiledit_cant_save'] = 'این فایل قابل ویرایش نیست. لطفا بررسی کنید که آیا پوشه‌ی «local/» دسترسی نوشتن دارد یا خیر؟ (chmod)';
 $lang['locfiledit_choose_file'] = 'یک فایل را برای ویرایش انتخاب کنید';
 $lang['locfiledit_edit'] = 'ویرایش';
 $lang['locfiledit_empty_filename'] = 'باید یک نام برای فایل خود انتخاب کنید.';
-$lang['locfiledit_empty_page'] = 'برگه ی خالی';
-$lang['locfiledit_file_already_exists'] = 'فایلی با این نام موجود می باشد.';
-$lang['locfiledit_filename_error'] = 'نویسه های غیر مجاز در نام فایل استفاده شده است.';
-$lang['locfiledit_model_error'] = 'باید یک الگو را انتخاب کنید.';
-$lang['locfiledit_model'] = 'الگو';
+$lang['locfiledit_empty_page'] = 'برگه‌ی خالی';
+$lang['locfiledit_file_already_exists'] = 'فایلی با این نام از قبل موجود نیست.';
+$lang['locfiledit_filename_error'] = 'کاراکترهای غیر مجاز در نام فایل استفاده شده‌اند.';
+$lang['locfiledit_model_error'] = 'باید یک مدل انتخاب کنید.';
+$lang['locfiledit_model'] = 'مدل';
 $lang['locfiledit_new_filename'] = 'نام فایل';
 $lang['locfiledit_new_tpl'] = 'ایجاد فایل تازه';
-$lang['locfiledit_newfile'] = 'این فایل وجود ندارد و بوسیله ی LocalFiles Editor ایجاد خواهد شد';
+$lang['locfiledit_newfile'] = 'تا زمانی که اطلاعاتی وارد نکنید، این فایل ایجاد نخواهد شد. پس از ورود اطلاعات و ذخیره کردن، فایل ایجاد خواهد شد.';
 $lang['locfiledit_onglet_css'] = 'CSS';
-$lang['locfiledit_onglet_lang'] = 'زبان ها';
-$lang['locfiledit_onglet_localconf'] = 'پیکربندی محلی (Local Config)';
-$lang['locfiledit_onglet_plug'] = 'افزونه های شخصی';
-$lang['locfiledit_onglet_tpl'] = 'قالب ها';
-$lang['locfiledit_parent_directory'] = 'پوشه ی مادر';
-$lang['locfiledit_restore_confirm'] = 'آیا مطمئن می باشید؟
-<br>
-بدون ذخیره ی فایل بازیابی آن ممکن نخواهد بود.';
+$lang['locfiledit_onglet_lang'] = 'زبان‌ها';
+$lang['locfiledit_onglet_localconf'] = 'پیکربندی محلی';
+$lang['locfiledit_onglet_plug'] = 'افزونه‌ی شخصی';
+$lang['locfiledit_onglet_tpl'] = 'قالب‌ها';
+$lang['locfiledit_parent_directory'] = 'پوشه‌ی مادر';
+$lang['locfiledit_restore_confirm'] = 'آیا از انجام این کار اطمینان دارید؟
+بدون ذخیره‌ی فایل، بازیابی آن ممکن نخواهد بود.';
 $lang['locfiledit_restore'] = 'بازیابی فایل پشتیبان';
-$lang['locfiledit_save_bak'] = 'فایل پشتیبان پس از نخستین ذخیره ایجاد خواهد شد.';
-$lang['locfiledit_save_config'] = 'فایل با موفقیت ویرایش شد.';
-$lang['locfiledit_save_file'] = 'ذخیره فایل';
+$lang['locfiledit_save_bak'] = 'فایل پشتیبان پس از ذخیره ایجاد خواهد شد.';
+$lang['locfiledit_save_config'] = 'فایل با موفقیت نوشته شد.';
+$lang['locfiledit_save_file'] = 'ذخیره‌ی فایل';
 $lang['locfiledit_saved_bak'] = 'فایل پشتیبان ایجاد شد (%s)';
-$lang['locfiledit_show_default'] = 'نمایش فایل مرجع';
-$lang['locfiledit_syntax_error'] = 'خطای دستوری (Syntax)! امکان ذخیره ی فایل وجود ندارد.';
-$lang['locfiledit_webmaster_only'] = 'فقط مدیرکل می تواند یک فایل محلی را ایجاد یا ویرایش کند.';
-?>
+$lang['locfiledit_show_default'] = 'نمایش فایل مرجع:';
+$lang['locfiledit_syntax_error'] = 'خطای دستوری (Syntax)! امکان ذخیره‌ی فایل وجود ندارد.';
+$lang['locfiledit_webmaster_only'] = 'فقط مدیرکل می‌تواند یک فایل محلی را ایجاد یا ویرایش کند.';
+$lang['Customize CSS'] = 'سفارشی سازی CSS';
