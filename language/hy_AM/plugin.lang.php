5f882e2066eec434378cda6e00313b61d64eda50
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-14 08:39:18 +0100

    Update hy_AM, thanks to gayane

---
+++ language/hy_AM/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_parent_directory'] = 'Արմատական գրացուցակ';
+$lang['locfiledit_restore'] = 'Վերականգնել պահուստային ֆայլը';
+$lang['locfiledit_restore_confirm'] = 'Խնդրում ենք հաստատել:
+Վերականգնումը արդյունավետ չի լինի մինչև հաջորդ պահպանումը:';
+$lang['locfiledit_save_bak'] = 'Կրկնօրինակը կստեղծվի պահպանման ժամանակ:';
+$lang['locfiledit_save_config'] = 'Ֆայլը հաջողությամբ գրված է:';
+$lang['locfiledit_save_file'] = 'Պահպանել ֆայլը';
+$lang['locfiledit_saved_bak'] = 'Պահուստային ֆայլը ստեղծվել է (%s)';
+$lang['locfiledit_show_default'] = 'Ցուցադրել ռեֆեռենս ֆայլը:';
+$lang['locfiledit_syntax_error'] = 'Շարահյուսական սխալ՝ ֆայլը Հնարավոր չէ պահպանել:';
+$lang['locfiledit_webmaster_only'] = 'Միայն տեխ. մասնագետները կարող են ստեղծել կամ փոփոխել լոկալ ֆայլերը:';
+$lang['Customize CSS'] = 'CSS-ի կարգավորում';
+$lang['locfiledit_bak_loaded1'] = 'Պահուստային ֆայլը բեռնված է:';
+$lang['locfiledit_bak_loaded2'] = 'Դուք պետք է պահպանեք ֆայլը այն վերականգնել համար:';
+$lang['locfiledit_cant_save'] = 'Ընթացիկ ֆայլը գրելու ենթակա չէ: Ստուգեք՝ արդյոք "local/"-ը գրելու ենթակա է (chmod):';
+$lang['locfiledit_choose_file'] = 'Ընտրեք ֆայլը խմբագրելու համար';
+$lang['locfiledit_edit'] = 'Խմբագրել';
+$lang['locfiledit_empty_filename'] = 'Դուք պետք է մուտքագրեք ֆայլի անուն:';
+$lang['locfiledit_empty_page'] = 'Դատարկ էջ';
+$lang['locfiledit_file_already_exists'] = 'Ֆայլն արդեն գոյություն ունի:';
+$lang['locfiledit_filename_error'] = 'Արգելված նիշեր ֆայլի անվան մեջ:';
+$lang['locfiledit_model'] = 'Մոդել';
+$lang['locfiledit_model_error'] = 'Դուք պետք է ընտրեք մոդել:';
+$lang['locfiledit_new_filename'] = 'Ֆայլի անուն';
+$lang['locfiledit_new_tpl'] = 'Ստեղծել նոր ֆայլ';
+$lang['locfiledit_newfile'] = 'Ֆայլը գոյություն չունի, քանի դեռ որոշ տվյալներ մուտքագրված չեն: Երբ տվյալները մուտքագրվեն և պահպանվեն, ֆայլը կստեղծվի:';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = 'Լեզուներ';
+$lang['locfiledit_onglet_localconf'] = 'Լոկալ կազմաձևում';
+$lang['locfiledit_onglet_plug'] = 'Անձնական հավելում';
+$lang['locfiledit_onglet_tpl'] = 'Ձևանմուշներ';
