24d9457d1672959b8ffe906b60a5d12eb2458a34
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-31 06:47:20 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['locfiledit_parent_directory'] = '상위 디렉터리';
+$lang['locfiledit_restore'] = '백업 파일 복원';
+$lang['locfiledit_restore_confirm'] = '확인하시겠습니까?
+복원은 다음 저장 시까지 적용되지 않습니다.';
+$lang['locfiledit_save_bak'] = '저장 시 백업 사본이 생성됩니다.';
+$lang['locfiledit_save_config'] = '파일이 정상적으로 작성되었습니다.';
+$lang['locfiledit_save_file'] = '파일 저장';
+$lang['locfiledit_saved_bak'] = '백업 파일 생성됨 (%s)';
+$lang['locfiledit_show_default'] = '참조 파일 표시: ';
+$lang['locfiledit_syntax_error'] = '구문 오류! 파일을 저장할 수 없습니다.';
+$lang['locfiledit_webmaster_only'] = '로컬 파일 생성 또는 수정은 웹마스터만 가능합니다.';
+$lang['Customize CSS'] = 'CSS 사용자 정의';
+$lang['locfiledit_bak_loaded1'] = '백업 파일 로드됨.';
+$lang['locfiledit_bak_loaded2'] = '복원하려면 파일을 저장해야 합니다.';
+$lang['locfiledit_cant_save'] = '현재 파일은 쓰기 권한이 없습니다. "local/" 디렉터리의 쓰기 권한(chmod)을 확인하세요.';
+$lang['locfiledit_choose_file'] = '편집할 파일 선택';
+$lang['locfiledit_edit'] = '편집';
+$lang['locfiledit_empty_filename'] = '파일 이름을 입력해야 합니다.';
+$lang['locfiledit_empty_page'] = '빈 페이지';
+$lang['locfiledit_file_already_exists'] = '파일이 이미 존재합니다.';
+$lang['locfiledit_filename_error'] = '파일 이름에 허용되지 않는 문자가 포함되었습니다.';
+$lang['locfiledit_model'] = '모델';
+$lang['locfiledit_model_error'] = '모델을 선택해야 합니다.';
+$lang['locfiledit_new_filename'] = '파일 이름';
+$lang['locfiledit_new_tpl'] = '새 파일 생성';
+$lang['locfiledit_newfile'] = '아래에 정보를 입력하기 전까지는 파일이 생성되지 않습니다.
+정보를 입력하고 저장하면 파일이 생성됩니다.';
+$lang['locfiledit_onglet_css'] = 'CSS';
+$lang['locfiledit_onglet_lang'] = '언어';
+$lang['locfiledit_onglet_localconf'] = '로컬 설정';
+$lang['locfiledit_onglet_plug'] = '개인 플러그인';
+$lang['locfiledit_onglet_tpl'] = '템플릿';
